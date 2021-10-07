42ea6c4e09019f25ee6125ffc6110b180ff5f8cf
Eundaa <eundada064@gmail.com>
2021-10-07 23:35:01 +0900

    [2021.10.07] 프로그래머스 위클리 9주차 풀이

---
+++ Programmers/Weekly/Week9.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] sizes) {
+        int maxWidth = 0;
+        int[] heightList = new int[sizes.length];
+        for (int i = 0; i < sizes.length; i++) {
+            int minTemp = 1001;
+            for (int j = 0; j < 2; j++) {
+                if (maxWidth < sizes[i][j]) {
+                    maxWidth = sizes[i][j];
+                }
+                if (minTemp > sizes[i][j]) {
+                    minTemp = sizes[i][j];
+                }
+            }
+            heightList[i] = minTemp;
+        }
+        Arrays.sort(heightList);
+        int maxHeight = heightList[sizes.length - 1];
+        return maxWidth * maxHeight;
+    }
+}
